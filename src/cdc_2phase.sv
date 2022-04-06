773ba1035aa88f1d8df5270555f48067db8bb22e
Jakob Woehler <woehlerj@student.ethz.ch>
2022-04-06 20:51:08 +0200

    Update cdc_2phase

--- src/cdc_2phase.sv
+++ src/cdc_2phase.sv
@@ -21,12 +21,14 @@ module cdc_2phase #(
 )(
   input  logic src_rst_ni,
   input  logic src_clk_i,
+  input  logic src_clr_i,
   input  T     src_data_i,
   input  logic src_valid_i,
   output logic src_ready_o,
 
   input  logic dst_rst_ni,
   input  logic dst_clk_i,
+  input  logic dst_clr_i,
   output T     dst_data_o,
   output logic dst_valid_o,
   input  logic dst_ready_i
@@ -41,6 +43,7 @@ module cdc_2phase #(
   cdc_2phase_src #(.T(T)) i_src (
     .rst_ni       ( src_rst_ni  ),
     .clk_i        ( src_clk_i   ),
+    .clr_i        ( src_clr_i   ),
     .data_i       ( src_data_i  ),
     .valid_i      ( src_valid_i ),
     .ready_o      ( src_ready_o ),
@@ -53,6 +56,7 @@ module cdc_2phase #(
   cdc_2phase_dst #(.T(T)) i_dst (
     .rst_ni       ( dst_rst_ni  ),
     .clk_i        ( dst_clk_i   ),
+    .clr_i        ( dst_clr_i   ),
     .data_o       ( dst_data_o  ),
     .valid_o      ( dst_valid_o ),
     .ready_i      ( dst_ready_i ),
@@ -70,6 +74,7 @@ module cdc_2phase_src #(
 )(
   input  logic rst_ni,
   input  logic clk_i,
+  input  logic clr_i,
   input  T     data_i,
   input  logic valid_i,
   output logic ready_o,
@@ -82,28 +87,13 @@ module cdc_2phase_src #(
   logic req_src_q, ack_src_q, ack_q;
   (* dont_touch = "true" *)
   T data_src_q;
-
-  // The req_src and data_src registers change when a new data item is accepted.
-  always_ff @(posedge clk_i or negedge rst_ni) begin
-    if (!rst_ni) begin
-      req_src_q  <= 0;
-      data_src_q <= '0;
-    end else if (valid_i && ready_o) begin
-      req_src_q  <= ~req_src_q;
-      data_src_q <= data_i;
-    end
-  end
+  
+  `FFCIL(req_src_q, ~req_src_q, 0, clk_i, rst_ni, clr_i, (valid_i && ready_o))
+  `FFCIL(data_src_q, data_i, '0, clk_i, rst_ni, clr_i, (valid_i && ready_o))
 
   // The ack_src and ack registers act as synchronization stages.
-  always_ff @(posedge clk_i or negedge rst_ni) begin
-    if (!rst_ni) begin
-      ack_src_q <= 0;
-      ack_q     <= 0;
-    end else begin
-      ack_src_q <= async_ack_i;
-      ack_q     <= ack_src_q;
-    end
-  end
+  `FFC(ack_src_q, async_ack_i, 0, clk_i, rst_ni, clr_i)
+  `FFC(ack_q, ack_src_q, 0, clk_i, rst_ni, clr_i)
 
   // Output assignments.
   assign ready_o = (req_src_q == ack_q);
@@ -120,6 +110,7 @@ module cdc_2phase_dst #(
 )(
   input  logic rst_ni,
   input  logic clk_i,
+  input  logic clr_i,
   output T     data_o,
   output logic valid_o,
   input  logic ready_i,
@@ -135,36 +126,17 @@ module cdc_2phase_dst #(
   T data_dst_q;
 
   // The ack_dst register changes when a new data item is accepted.
-  always_ff @(posedge clk_i or negedge rst_ni) begin
-    if (!rst_ni) begin
-      ack_dst_q  <= 0;
-    end else if (valid_o && ready_i) begin
-      ack_dst_q  <= ~ack_dst_q;
-    end
-  end
+  `FFCIL(ack_dst_q, ~ack_dst_q, 0, clk_i, rst_ni, clr_i, (valid_o && ready_i))
 
   // The data_dst register changes when a new data item is presented. This is
   // indicated by the async_req line changing levels.
-  always_ff @(posedge clk_i or negedge rst_ni) begin
-    if (!rst_ni) begin
-      data_dst_q <= '0;
-    end else if (req_q0 != req_q1 && !valid_o) begin
-      data_dst_q <= async_data_i;
-    end
-  end
+  `FFCIL(data_dst_q, async_data_i, '0, clk_i, rst_ni, (req_q0 != req_q1 && !valid_o))
 
   // The req_dst and req registers act as synchronization stages.
-  always_ff @(posedge clk_i or negedge rst_ni) begin
-    if (!rst_ni) begin
-      req_dst_q <= 0;
-      req_q0    <= 0;
-      req_q1    <= 0;
-    end else begin
-      req_dst_q <= async_req_i;
-      req_q0    <= req_dst_q;
-      req_q1    <= req_q0;
-    end
-  end
+  `FFC(req_dst_q, async_req_i, 0, clk_i, rst_ni, clr_i)
+  `FFC(req_q0, req_dst_q, 0, clk_i, rst_ni, clr_i)
+  `FFC(req_q1, req_q0, 0, clk_i, rst_ni, clr_i)
+
 
   // Output assignments.
   assign valid_o = (ack_dst_q != req_q1);
