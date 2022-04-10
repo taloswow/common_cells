7f0c83914eb60efad6a76cda6d984894cee2dc09
Jakob Wöhler <jakob.woehler@gmail.com>
2022-04-10 12:07:05 +0200

    Update sync

--- src/sync.sv
+++ src/sync.sv
@@ -10,12 +10,15 @@
 
 // Antonio Pullini <pullinia@iis.ee.ethz.ch>
 
+`include "common_cells/registers.svh"
+
 module sync #(
     parameter int unsigned STAGES = 2,
     parameter bit ResetValue = 1'b0
 ) (
     input  logic clk_i,
     input  logic rst_ni,
+    input  logic clr_i,
     input  logic serial_i,
     output logic serial_o
 );
@@ -24,13 +27,7 @@ module sync #(
    (* async_reg = "true" *)
    logic [STAGES-1:0] reg_q;
 
-    always_ff @(posedge clk_i, negedge rst_ni) begin
-        if (!rst_ni) begin
-            reg_q <= {STAGES{ResetValue}};
-        end else begin
-            reg_q <= {reg_q[STAGES-2:0], serial_i};
-        end
-    end
+    `FFC(reg_q, ({reg_q[STAGES-2:0], serial_i}), ({reg_q[STAGES-2:0], serial_i}), clk_i, rst_ni, clr_i)
 
     assign serial_o = reg_q[STAGES-1];
 
